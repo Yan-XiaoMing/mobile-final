32074b29326002111494959365905be3946408b1
paperGangsta <1024744235@qq.com>
2021-05-15 10:51:13 +0800

    config antd

--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals'
+import { ConfigProvider} from 'antd';
 import './reset.css';
+import zhCN from 'antd/lib/locale/zh_CN';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+import 'antd/dist/antd.css';
+import './index.css';
+
+moment.locale('zh-cn');
 
 ReactDOM.render(
   <React.StrictMode>
+      <ConfigProvider locale={zhCN}>
       <App />
+      </ConfigProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
